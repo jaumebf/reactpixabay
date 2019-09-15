701457b07828521e33a36b62041ff827b8131697
jaume <jaumewowcore>
2019-09-15 18:30:57 +0200

    Proyecto basico en React
    Rect con api de pixabay impartido por Codigo Juan, https://www.youtube.com/watch?v=hScR513gvNo&t=2251s

--- src/App.js
+++ src/App.js
@@ -1,26 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Buscador from './componentes/Buscador';
+import Resultado from './componentes/Resultado';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    termino: 'Café',
+    imagenes: [],
+    pagina: ''
+  }
+
+  scroll = () => {
+    const elemento = document.querySelector('.jumbotron');
+    elemento.scrollIntoView('smooth', 'start');
+  }
+
+  paginaAnterior = () => {
+    //Leer state pagina actual
+    let pagina = this.state.pagina;
+
+    //Leer si la pagina es 1, no ir atras
+    if (pagina === 1) return null;
+
+    //Restar 1 a la pagina actual
+    pagina--;
+
+    //Agregar cambio al state
+    this.setState({
+      pagina: pagina
+    }, () => {
+      this.consultarApi();
+      this.scroll();
+    });
+    
+    // console.log(pagina);
+  }
+  
+  paginaSiguiente = () => {
+    //Leer state pagina actual
+    let pagina = this.state.pagina;
+    
+    //Sumar 1 a la pagina actual
+    pagina++;
+    
+    //Agregar cambio al state
+    this.setState({
+      pagina: pagina
+    }, () => {
+      this.consultarApi();
+      this.scroll();
+    });
+
+    // console.log(pagina);
+  }
+
+  consultarApi = () => {
+    let api_key = "13624024-ceb12e8afa50e5bb05d0c7390";
+    const pagina = this.state.pagina;
+    const url = `https://pixabay.com/api/?key=${api_key}&q=${this.state.termino}
+    &per_page=20&page=${pagina}`;
+    // console.log(url);
+
+    fetch(url)
+      .then(respuesta => respuesta.json())
+      .then(resultado => this.setState({ imagenes: resultado.hits }))
+  }
+
+  datosBusqueda = (termino) => {
+    this.setState({
+      termino: termino,
+      pagina: 1
+    }, () => {
+      this.consultarApi();
+    })
+  }
+
+  render() {
+    return (
+      <div className="app container">
+        <div className="jumbotron">
+          <p className="lead text-center">Buscador de imagenes</p>
+          <Buscador
+            datosBusqueda={this.datosBusqueda}
+          />
+        </div>
+        <div className="row justify-content-center">
+          <Resultado
+            imagenes={this.state.imagenes}
+            paginaAnterior={this.paginaAnterior}
+            paginaSiguiente={this.paginaSiguiente}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
